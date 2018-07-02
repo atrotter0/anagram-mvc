57c8a1de335a986879e34a8eac3df6036972b27a
Abel Trotter and Ryan Putman <abel@>
2018-07-02 13:28:00 -0700

    Refactor for use with MVC

--- WordGame.Tests/ModelTests/Anagram.Tests.cs
+++ WordGame.Tests/ModelTests/Anagram.Tests.cs
@@ -24,90 +24,80 @@ namespace WordGame.Tests
             Assert.AreEqual("This is a word list.", newGame.GetWordList());
         }
 
-        //LEGACY TESTS
-        // [TestMethod]
-        // public void GetSetUserWords_GetsSetsUserWords_String()
-        // {
-        //     Anagram testAnagram = new Anagram();
-        //     string word = "test";
-        //     testAnagram.SetUserWords(word);
-        //     Assert.AreEqual(word, testAnagram.GetUserWords()[0]);
-        // }
-        //
-        // [TestMethod]
-        // public void GetSetWordMatches_GetsSetsWordMatches_String()
-        // {
-        //     Anagram testAnagram = new Anagram();
-        //     string word = "test";
-        //     testAnagram.SetWordMatches(word);
-        //     Assert.AreEqual(word, testAnagram.GetWordMatches()[0]);
-        // }
-        //
-        // [TestMethod]
-        // public void GetIncrementCharCounter_GetsIncrementsWordCounter_Int()
-        // {
-        //     Anagram testAnagram = new Anagram();
-        //     testAnagram.IncrementCounter();
-        //     Assert.AreEqual(1, testAnagram.GetCounter());
-        // }
-        //
-        // [TestMethod]
-        // public void GetSetSameLetters_SameLettersTrue_True()
-        // {
-        //     Anagram testAnagram = new Anagram();
-        //     testAnagram.SameLettersTrue();
-        //     Assert.AreEqual(true, testAnagram.GetSameLetters());
-        // }
-        //
-        // [TestMethod]
-        // public void GetSetWordToCompare_SetsWordToCompare_String()
-        // {
-        //     Anagram testAnagram = new Anagram();
-        //     string word = "test";
-        //     testAnagram.SetWordToCompare(word);
-        //     Assert.AreEqual(word, testAnagram.GetWordToCompare());
-        // }
-        //
-        // [TestMethod]
-        // public void CharactersInWord_WordTurnedIntoCharArray_CharArray()
-        // {
-        //     Anagram testAnagram = new Anagram();
-        //     string word = "noob";
-        //     char[] wordSplit = { 'n', 'o', 'o', 'b' };
-        //     CollectionAssert.AreEqual(wordSplit, testAnagram.CharactersInWord(word));
-        // }
-        //
-        // [TestMethod]
-        // public void AddAnagram_AddsWordsToMatchList_String()
-        // {
-        //     Anagram testAnagram = new Anagram();
-        //     string word = "gandalf";
-        //     testAnagram.SetWordToCompare(word);
-        //     testAnagram.SameLettersTrue();
-        //     testAnagram.AddAnagram();
-        //     Assert.AreEqual(word, testAnagram.GetWordMatches()[0]);
-        // }
-        //
-        // [TestMethod]
-        // public void CheckLettersAreEqual_TestEachLetterIsEqual_True()
-        // {
-        //     Anagram testAnagram = new Anagram();
-        //     char letterOne = 'f';
-        //     char letterTwo = 'f';
-        //     testAnagram.CheckLettersAreEqual(letterOne, letterTwo);
-        //     Assert.AreEqual(true, testAnagram.GetSameLetters());
-        // }
-        //
-        // [TestMethod]
-        // public void SortAndReturnWord_CharsAreSortedAndReturned_CharArray()
-        // {
-        //     Anagram testAnagram = new Anagram();
-        //     string word = "frodo";
-        //     char[] splitWord = word.ToCharArray();
-        //     Array.Sort(splitWord);
-        //     CollectionAssert.AreEqual(splitWord, testAnagram.SortAndReturnChars(word));
-        // }
-        //
+        [TestMethod]
+        public void GetSetWordMatches_GetsSetsWordMatches_String()
+        {
+            Anagram testAnagram = new Anagram();
+            string word = "test";
+            testAnagram.SetWordMatches(word);
+            Assert.AreEqual(word, testAnagram.GetWordMatches()[0]);
+        }
+
+        [TestMethod]
+        public void GetIncrementCharCounter_GetsIncrementsWordCounter_Int()
+        {
+            Anagram testAnagram = new Anagram();
+            testAnagram.IncrementCounter();
+            Assert.AreEqual(1, testAnagram.GetCounter());
+        }
+
+        [TestMethod]
+        public void GetSetSameLetters_SameLettersTrue_True()
+        {
+            Anagram testAnagram = new Anagram();
+            testAnagram.SameLettersTrue();
+            Assert.AreEqual(true, testAnagram.GetSameLetters());
+        }
+
+        [TestMethod]
+        public void GetSetWordToCompare_SetsWordToCompare_String()
+        {
+            Anagram testAnagram = new Anagram();
+            string word = "test";
+            testAnagram.SetWordToCompare(word);
+            Assert.AreEqual(word, testAnagram.GetWordToCompare());
+        }
+
+        [TestMethod]
+        public void CharactersInWord_WordTurnedIntoCharArray_CharArray()
+        {
+            Anagram testAnagram = new Anagram();
+            string word = "noob";
+            char[] wordSplit = { 'n', 'o', 'o', 'b' };
+            CollectionAssert.AreEqual(wordSplit, testAnagram.CharactersInWord(word));
+        }
+
+        [TestMethod]
+        public void AddAnagram_AddsWordsToMatchList_String()
+        {
+            Anagram testAnagram = new Anagram();
+            string word = "gandalf";
+            testAnagram.SetWordToCompare(word);
+            testAnagram.SameLettersTrue();
+            testAnagram.AddAnagram();
+            Assert.AreEqual(word, testAnagram.GetWordMatches()[0]);
+        }
+
+        [TestMethod]
+        public void CheckLettersAreEqual_TestEachLetterIsEqual_True()
+        {
+            Anagram testAnagram = new Anagram();
+            char letterOne = 'f';
+            char letterTwo = 'f';
+            testAnagram.CheckLettersAreEqual(letterOne, letterTwo);
+            Assert.AreEqual(true, testAnagram.GetSameLetters());
+        }
+
+        [TestMethod]
+        public void SortAndReturnWord_CharsAreSortedAndReturned_CharArray()
+        {
+            Anagram testAnagram = new Anagram();
+            string word = "frodo";
+            char[] splitWord = word.ToCharArray();
+            Array.Sort(splitWord);
+            CollectionAssert.AreEqual(splitWord, testAnagram.SortAndReturnChars(word));
+        }
+
         // [TestMethod]
         // public void CheckForAnagram_AddAnagramToMatchList_List()
         // {
--- WordGame/Models/Anagram.cs
+++ WordGame/Models/Anagram.cs
@@ -7,6 +7,10 @@ namespace WordGame.Models
     {
         private string _baseWord;
         private string _wordList;
+        private int _charCounter = 0;
+        private bool _sameLetters = false;
+        private List<string> _wordMatches = new List<string>() {};
+        private string _wordToCompare = "";
 
         public void SetBaseWord(string newWord)
         {
@@ -28,11 +32,61 @@ namespace WordGame.Models
             return _wordList;
         }
 
+        public void IncrementCounter()
+        {
+            _charCounter++;
+        }
+
+        public int GetCounter()
+        {
+            return _charCounter;
+        }
+
+        public void SameLettersTrue()
+        {
+            _sameLetters = true;
+        }
+
+        public bool GetSameLetters()
+        {
+            return _sameLetters;
+        }
+
+        public void SetWordMatches(string word)
+        {
+            this.GetWordMatches().Add(word);
+        }
+
+        public List<string> GetWordMatches()
+        {
+            return _wordMatches;
+        }
+
+        public string GetWordToCompare()
+        {
+            return _wordToCompare;
+        }
+
+        public void SetWordToCompare(string word)
+        {
+            _wordToCompare = word;
+        }
+
         // write new test for this
         public void FindAnagram()
         {
             char[] firstWordLetters = this.SortAndReturnChars(this.GetBaseWord());
-            // go through word list
+            string[] words = this.GetWordList().Split(' ');
+            foreach (string word in words)
+            {
+                this.SetWordToCompare(word);
+                char[] secondWordLetters = this.SortAndReturnChars(word);
+                if (secondWordLetters.Length == firstWordLetters.Length)
+                {
+                  this.CheckLettersAreEqual(firstWordLetters[this.GetCounter()], secondWordLetters[this.GetCounter()]);
+                  this.IncrementCounter();
+                }
+            }
         }
 
         public char[] SortAndReturnChars(string word)
@@ -47,117 +101,18 @@ namespace WordGame.Models
             return word.ToCharArray();
         }
 
-        // public void CheckForAnagram()
-        // {
-        //     char[] firstWordLetters = this.SortAndReturnChars(this.GetUserWords()[0]);
-        //     for(int j = 1; j < this.GetUserWords().Count; j++)
-        //     {
-        //         this.SetWordToCompare(GetUserWords()[j]);
-        //         char[] secondWordLetters = this.SortAndReturnChars(this.GetUserWords()[j]);
-        //         if (secondWordLetters.Length == firstWordLetters.Length)
-        //         {
-        //             this.CheckLettersAreEqual(firstWordLetters[this.GetCounter()], secondWordLetters[this.GetCounter()]);
-        //             IncrementCounter();
-        //         }
-        //     }
-        // }
+        public void CheckLettersAreEqual(char letterOne, char letterTwo)
+        {
+            if (letterOne == letterTwo)
+            {
+              this.SameLettersTrue();
+              this.AddAnagram();
+            }
+        }
 
-        // LEGACY CODE
-        // private List<string> _userWords = new List<string>() {};
-        // private List<string> _wordMatches = new List<string>() {};
-        // private int _charCounter = 0;
-        // private bool _sameLetters = false;
-        // private string _wordToCompare = "";
-
-        // public void SetUserWords(string word)
-        // {
-        //     this.GetUserWords().Add(word);
-        // }
-        //
-        // public List<string> GetUserWords()
-        // {
-        //     return _userWords;
-        // }
-        //
-        // public void SetWordMatches(string word)
-        // {
-        //     this.GetWordMatches().Add(word);
-        // }
-        //
-        // public List<string> GetWordMatches()
-        // {
-        //     return _wordMatches;
-        // }
-        //
-        // public void IncrementCounter()
-        // {
-        //     _charCounter++;
-        // }
-        //
-        // public int GetCounter()
-        // {
-        //     return _charCounter;
-        // }
-        //
-        // public void SameLettersTrue()
-        // {
-        //     _sameLetters = true;
-        // }
-        //
-        // public bool GetSameLetters()
-        // {
-        //     return _sameLetters;
-        // }
-        //
-        // public string GetWordToCompare()
-        // {
-        //     return _wordToCompare;
-        // }
-        //
-        // public void SetWordToCompare(string word)
-        // {
-        //     _wordToCompare = word;
-        // }
-
-        // public void CheckForAnagram()
-        // {
-        //     char[] firstWordLetters = this.SortAndReturnChars(this.GetUserWords()[0]);
-        //     for(int j = 1; j < this.GetUserWords().Count; j++)
-        //     {
-        //         this.SetWordToCompare(GetUserWords()[j]);
-        //         char[] secondWordLetters = this.SortAndReturnChars(this.GetUserWords()[j]);
-        //         if (secondWordLetters.Length == firstWordLetters.Length)
-        //         {
-        //             this.CheckLettersAreEqual(firstWordLetters[this.GetCounter()], secondWordLetters[this.GetCounter()]);
-        //             IncrementCounter();
-        //         }
-        //     }
-        // }
-        //
-        // public char[] SortAndReturnChars(string word)
-        // {
-        //     char[] lettersOfWord = this.CharactersInWord(word);
-        //     Array.Sort(lettersOfWord);
-        //     return lettersOfWord;
-        // }
-        //
-        // public void CheckLettersAreEqual(char letterOne, char letterTwo)
-        // {
-        //     if (letterOne == letterTwo)
-        //     {
-        //       SameLettersTrue();
-        //       this.AddAnagram();
-        //     }
-        // }
-        //
-        // public void AddAnagram()
-        // {
-        //     if (this.GetSameLetters()) this.SetWordMatches(this.GetWordToCompare());
-        // }
-        //
-        // public char[] CharactersInWord(string word)
-        // {
-        //     return word.ToCharArray();
-        // }
+        public void AddAnagram()
+        {
+            if (this.GetSameLetters()) this.SetWordMatches(this.GetWordToCompare());
+        }
     }
 }
